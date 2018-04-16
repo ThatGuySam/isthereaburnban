aa57a2a6ef65629c0ad004ef364a84177b34d864
Sam Carlton <sam@sam.lc>
2018-04-16 18:44:14 -0500

    Setup now-env

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+/**
+ * After the next require you can use process.env to get your secrets
+ */
+require('now-env')
+
+console.log({
+  SECRET: process.env.IPSTACK_KEY
+})
+
+/**
+ * If some of the envs are public, like a google maps key, but you still
+ * want to keep them secret from the repo, the following code will allow you
+ * to share some variables with the client, configured at compile time.
+ */
+module.exports = {
+  webpack: config => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.IPSTACK_KEY': JSON.stringify(process.env.IPSTACK_KEY)
+      })
+      // Same as above
+      // new webpack.EnvironmentPlugin(['SECRET'])
+    )
+    return config
+  }
+}
