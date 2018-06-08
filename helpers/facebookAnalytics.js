640e66e7d68beef1f405951a41052f27dedefe9a
Sam Carlton <sam@sam.lc>
2018-06-07 22:54:05 -0500

    Modify facebookAnalytics to pass jest

--- helpers/facebookAnalytics.js
+++ helpers/facebookAnalytics.js
@@ -12,10 +12,11 @@ export default () => {
   const id = 'facebook-jssdk'
   const fbAppId = process.env.FACEBOOK_APP_ID
   
-  let js, fjs = document.getElementsByTagName('script')[0]
+  let js
+  let fjs = document.getElementsByTagName('script')[0]
   if (document.getElementById(id)) return
   js = document.createElement('script')
   js.id = id
   js.src = `//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=${fbAppId}`
-  fjs.parentNode.insertBefore(js, fjs)
+  if (typeof fjs !== 'undefined') fjs.parentNode.insertBefore(js, fjs)
 }
