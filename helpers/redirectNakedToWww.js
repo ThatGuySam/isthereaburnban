042dc8007801777e0789794f68c04874b198f991
Sam Carlton <sam@sam.lc>
2018-06-14 12:25:54 -0500

    Add path forwarding to url redirect

--- helpers/redirectNakedToWww.js
+++ helpers/redirectNakedToWww.js
@@ -1,8 +1,9 @@
 module.exports = (req, res, next) => {
+  console.log('req.originalUrl', req.originalUrl)
   // if the request host doesn't have www
   if (req.hostname === 'isthereaburnban.com') {
     // redirect to mycompany.com keeping the pathname and querystring
-    return res.redirect('https://www.isthereaburnban.com')
+    return res.redirect(`https://www.isthereaburnban.com${req.originalUrl}`)
   }
   return next() // call the next middleware (or route)
 }
