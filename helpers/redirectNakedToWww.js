057959537d633816597c8477eafdacfda95ec5ba
Sam Carlton <sam@sam.lc>
2018-06-14 12:09:46 -0500

    Fix redirect condition backwards

--- helpers/redirectNakedToWww.js
+++ helpers/redirectNakedToWww.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   // if the request host doesn't have www
-  if (req.hostname !== 'isthereaburnban.com') {
+  if (req.hostname === 'isthereaburnban.com') {
     // redirect to mycompany.com keeping the pathname and querystring
     return res.redirect('https://www.isthereaburnban.com')
   }
