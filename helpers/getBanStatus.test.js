38514b1a654d18d836024744c0cd301c21e2a036
Sam Carlton <sam@sam.lc>
2018-07-02 17:55:36 -0500

    Fix Improperly named broken arrow test

--- helpers/getBanStatus.test.js
+++ helpers/getBanStatus.test.js
@@ -17,8 +17,22 @@ describe('Check Bans', () => {
     })
   })
   
-  it('Can Check Broken Arrow', async () => {
+  it('Can Check Boise City', async () => {
     const geolocation = mockGeolocations('boisecity')
+    const burnBans = await getBanStatus(geolocation)
+
+    expect(burnBans).toMatchSnapshot({
+      'backgroundColor': expect.any(String),
+      'button': expect.any(Object),
+      'key': expect.any(String),
+      // 'location': expect.any(Object),
+      'message': expect.any(String),
+      'word': expect.any(String)
+    })
+  })
+  
+  it('Can Check Broken Arrow', async () => {
+    const geolocation = mockGeolocations('brokenarrow')
     const burnBans = await getBanStatus(geolocation)
 
     expect(burnBans).toMatchSnapshot({
